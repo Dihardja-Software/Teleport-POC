baea96b7ea65778ef00d6dafad233008b27ee386
lydia nathania <lydia@diharjasoftware.com>
2023-05-19 17:25:32 +0700

    living world home

--- src/App.js
+++ src/App.js
@@ -1,34 +1,150 @@
 import React from 'react'
 
-import './rectangle1.css'
+import Header from './header'
+import Footer from './footer'
+import './home.css'
 
-const Rectangle1 = (props) => {
+const Home = (props) => {
   return (
-    <div className="rectangle1-container">
-      <div className="rectangle1-dummy-container">
-        <img
-          alt="Rectangle112"
-          src="/playground_assets/Rectangle-1.png"
-          loading="lazy"
-          className="rectangle1-rectangle1"
-        />
-        <span className="rectangle1-text">
-          Hahahahahahah how to use this teleport? Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Cras eget tellus iaculis, convallis nisl quis,
-          iaculis ante. Phasellus eleifend pharetra rutrum. Nam in eros
-          eleifend, convallis velit vel, mollis est. Integer varius eu enim ac
-          varius. Sed dictum mi nisi, vel lacinia urna dignissim eget. Vivamus
-          feugiat velit nisl, vel dapibus urna maximus quis. Donec volutpat
-          auctor suscipit. Donec porta neque vitae egestas varius. Nam euismod
-          mauris ligula, a consequat urna pharetra in.
-        </span>
-        <span className="rectangle1-text1">
-          <span>testing title</span>
-        </span>
-        <button className="rectangle1-button button">Button</button>
+    <div className="home-container">
+      <Header rootClassName="header-root-class-name"></Header>
+      <div className="home-hero-image">
+        <div className="home-container01">
+          <img
+            alt="pastedImage"
+            src="/playground_assets/pastedimage-db1k-200w.png"
+            className="home-pasted-image"
+          />
+          <span className="home-text">
+            The Biggest Home Living and Eat-tertainment Center
+          </span>
+          <img
+            alt="pastedImage"
+            src="/playground_assets/pastedimage-vyvf-1500h.png"
+            className="home-pasted-image1"
+          />
+        </div>
       </div>
+      <div className="home-living-world">
+        <div className="home-container02">
+          <div className="home-container03">
+            <img
+              alt="pastedImage"
+              src="/playground_assets/pastedimage-nq7g-1200w.png"
+              className="home-pasted-image2"
+            />
+          </div>
+          <div className="home-container04">
+            <h1 className="home-text01">Living World</h1>
+            <div className="home-container05"></div>
+            <span className="home-text02">
+              <span>
+                Living World is a shopping center with &quot;Home Living,
+                Lifestyle &amp; Eat-ertainment&quot; concept, developed by Kawan
+                Lama Group with their broad experience in developing various
+                modern retail and shopping centers. Living World continues to
+                pursue its noble goal of improving sustainable communities and
+                the environment.
+              </span>
+              <br></br>
+              <br></br>
+              <span>
+                The first Living World started operating in Tangerang in 2011 as
+                &quot;The Biggest Home Living&quot;. The second Living World
+                opened in Pekanbaru, Riau, in 2018, known as &quot;The Biggest
+                Home Living in Sumatera&quot;.
+              </span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="home-live-card">
+        <div className="home-container06">
+          <div className="home-container07">
+            <h1 className="home-text07">Live Card</h1>
+            <div className="home-container08"></div>
+            <span className="home-text08">
+              Loyalty program that offers a lot of rewards and benefits for ever
+              member. Start from Green Membership, Blue Membership, to our
+              highest tier, Premiere Membership. Enjoy all the rewards by
+              shopping more at Living World &amp; Living Plaza.
+            </span>
+            <div className="home-container09">
+              <span className="home-text09">Join Live Membership</span>
+              <svg viewBox="0 0 1024 1024" className="home-icon">
+                <path d="M621.254 877.254l320-320c24.994-24.992 24.994-65.516 0-90.51l-320-320c-24.994-24.992-65.516-24.992-90.51 0-24.994 24.994-24.994 65.516 0 90.51l210.746 210.746h-613.49c-35.346 0-64 28.654-64 64s28.654 64 64 64h613.49l-210.746 210.746c-12.496 12.496-18.744 28.876-18.744 45.254s6.248 32.758 18.744 45.254c24.994 24.994 65.516 24.994 90.51 0z"></path>
+              </svg>
+            </div>
+          </div>
+          <div className="home-container10">
+            <img
+              alt="pastedImage"
+              src="/playground_assets/pastedimage-c0uc-1200w.png"
+              className="home-pasted-image3"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="home-container11">
+        <div className="home-container12">
+          <div className="home-container13">
+            <h1 className="home-text10">Download Our Mobile App Now!</h1>
+            <span className="home-text11">
+              Register yourself to our mobile app and join our membership. More
+              benefits and rewards are waiting for yo
+            </span>
+            <div className="home-container14">
+              <div className="home-container15">
+                <svg viewBox="0 0 1024 1024" className="home-icon02">
+                  <path d="M954.857 323.429c0 14.286-5.714 28.571-16 38.857l-491.429 491.429c-10.286 10.286-24.571 16-38.857 16s-28.571-5.714-38.857-16l-284.571-284.571c-10.286-10.286-16-24.571-16-38.857s5.714-28.571 16-38.857l77.714-77.714c10.286-10.286 24.571-16 38.857-16s28.571 5.714 38.857 16l168 168.571 374.857-375.429c10.286-10.286 24.571-16 38.857-16s28.571 5.714 38.857 16l77.714 77.714c10.286 10.286 16 24.571 16 38.857z"></path>
+                </svg>
+                <svg viewBox="0 0 1024 1024" className="home-icon04">
+                  <path d="M954.857 323.429c0 14.286-5.714 28.571-16 38.857l-491.429 491.429c-10.286 10.286-24.571 16-38.857 16s-28.571-5.714-38.857-16l-284.571-284.571c-10.286-10.286-16-24.571-16-38.857s5.714-28.571 16-38.857l77.714-77.714c10.286-10.286 24.571-16 38.857-16s28.571 5.714 38.857 16l168 168.571 374.857-375.429c10.286-10.286 24.571-16 38.857-16s28.571 5.714 38.857 16l77.714 77.714c10.286 10.286 16 24.571 16 38.857z"></path>
+                </svg>
+                <svg viewBox="0 0 1024 1024" className="home-icon06">
+                  <path d="M954.857 323.429c0 14.286-5.714 28.571-16 38.857l-491.429 491.429c-10.286 10.286-24.571 16-38.857 16s-28.571-5.714-38.857-16l-284.571-284.571c-10.286-10.286-16-24.571-16-38.857s5.714-28.571 16-38.857l77.714-77.714c10.286-10.286 24.571-16 38.857-16s28.571 5.714 38.857 16l168 168.571 374.857-375.429c10.286-10.286 24.571-16 38.857-16s28.571 5.714 38.857 16l77.714 77.714c10.286 10.286 16 24.571 16 38.857z"></path>
+                </svg>
+                <svg viewBox="0 0 1024 1024" className="home-icon08">
+                  <path d="M954.857 323.429c0 14.286-5.714 28.571-16 38.857l-491.429 491.429c-10.286 10.286-24.571 16-38.857 16s-28.571-5.714-38.857-16l-284.571-284.571c-10.286-10.286-16-24.571-16-38.857s5.714-28.571 16-38.857l77.714-77.714c10.286-10.286 24.571-16 38.857-16s28.571 5.714 38.857 16l168 168.571 374.857-375.429c10.286-10.286 24.571-16 38.857-16s28.571 5.714 38.857 16l77.714 77.714c10.286 10.286 16 24.571 16 38.857z"></path>
+                </svg>
+              </div>
+              <ul className="home-ul list">
+                <li className="home-li list-item">
+                  <span className="home-text12">
+                    Earn points every time you shop at Living World &amp; Living
+                    Plaza
+                  </span>
+                </li>
+                <li className="home-li1 list-item">
+                  <span className="home-text13">
+                    Exchange points with vouchers
+                  </span>
+                </li>
+                <li className="home-li2 list-item">
+                  <span className="home-text14">
+                    Never miss out latest events &amp; promotions
+                  </span>
+                </li>
+                <li className="home-li3 list-item">
+                  <span className="home-text15">
+                    Join in our Lucky Draw events
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="home-container16">
+            <img
+              alt="pastedImage"
+              src="/playground_assets/pastedimage-fcui-600h.png"
+              className="home-pasted-image4"
+            />
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
   )
 }
 
-export default Rectangle1
+export default Home
---
+++ src/header.js
@@ -0,0 +1,184 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+
+import './header.css'
+
+const Header = (props) => {
+  return (
+    <header className={`header-header ${props.rootClassName} `}>
+      <header data-thq="thq-navbar" className="header-navbar-interactive">
+        <img
+          alt={props.pastedImage_alt41}
+          src={props.pastedImage_src41}
+          className="header-pasted-image"
+        />
+        <div data-thq="thq-navbar-btn-group" className="header-btn-group"></div>
+        <div
+          data-thq="thq-navbar-nav"
+          data-role="Nav"
+          className="header-desktop-menu"
+        >
+          <nav
+            data-thq="thq-navbar-nav-links"
+            data-role="Nav"
+            className="header-nav"
+          >
+            <div className="header-container">
+              <span className="header-text">{props.text5}</span>
+              <span className="header-text01">{props.text11}</span>
+              <span className="header-text02">{props.text21}</span>
+              <span className="header-text03">{props.text31}</span>
+            </div>
+            <div className="header-container1">
+              <div className="header-container2">
+                <span className="header-text04">{props.text41}</span>
+                <img
+                  alt={props.pastedImage_alt51}
+                  src={props.pastedImage_src51}
+                  className="header-pasted-image1"
+                />
+              </div>
+              <img
+                alt={props.pastedImage_alt31}
+                src={props.pastedImage_src31}
+                className="header-pasted-image2"
+              />
+            </div>
+          </nav>
+        </div>
+        <div data-thq="thq-burger-menu" className="header-burger-menu">
+          <svg viewBox="0 0 1024 1024" className="header-icon">
+            <path
+              d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"
+              className=""
+            ></path>
+          </svg>
+        </div>
+        <div data-thq="thq-mobile-menu" className="header-mobile-menu">
+          <div
+            data-thq="thq-mobile-menu-nav"
+            data-role="Nav"
+            className="header-nav1"
+          >
+            <div className="header-container3">
+              <img
+                alt={props.pastedImage_alt411}
+                src={props.pastedImage_src411}
+                className="header-pasted-image3"
+              />
+              <div data-thq="thq-close-menu" className="header-menu-close">
+                <svg viewBox="0 0 1024 1024" className="header-icon2">
+                  <path
+                    d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"
+                    className=""
+                  ></path>
+                </svg>
+              </div>
+            </div>
+            <nav
+              data-thq="thq-mobile-menu-nav-links"
+              data-role="Nav"
+              className="header-nav2"
+            >
+              <span className="header-text05">Home</span>
+              <span className="header-text06">About Us</span>
+              <span className="header-text07">Our Malls</span>
+              <span className="header-text08">Our Projects</span>
+              <span className="header-text09">Language</span>
+              <span className="header-text10">My Profile</span>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  pastedImage_alt4: 'pastedImage',
+  pastedImage_src51: '/playground_assets/pastedimage-6zcn.svg',
+  pastedImage_alt411: 'pastedImage',
+  pastedImage_alt5: 'pastedImage',
+  pastedImage_alt51: 'pastedImage',
+  text31: 'Our Projects',
+  pastedImage_src1:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/e65bd9c6-9590-4e2a-bc7c-cb03db23dc7a?org_if_sml=15154',
+  pastedImage_alt31: 'pastedImage',
+  rootClassName: '',
+  pastedImage_alt41: 'pastedImage',
+  pastedImage_src4:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/9b4dfff1-a88e-4bf6-a602-e7ed11769844?org_if_sml=15391',
+  text5: 'Home',
+  pastedImage_src3:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/66579b52-3e75-4e05-aac9-5ea6cecee8a6?org_if_sml=12321',
+  pastedImage_alt311: 'pastedImage',
+  text11: 'About Us',
+  pastedImage_alt3: 'pastedImage',
+  text: 'Home',
+  text41: 'EN',
+  text3: 'Our Projects',
+  pastedImage_src31: '/playground_assets/pastedimage-nbw4.svg',
+  pastedImage_alt2: 'pastedImage',
+  text2: 'Our Malls',
+  text4: 'EN',
+  text21: 'Our Malls',
+  text1: 'About Us',
+  pastedImage_src311:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/66579b52-3e75-4e05-aac9-5ea6cecee8a6?org_if_sml=12321',
+  pastedImage_alt511: 'pastedImage',
+  pastedImage_src:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/d559717d-77ef-474b-8d3d-7d0426f391e2?org_if_sml=16689',
+  pastedImage_alt: 'pastedImage',
+  pastedImage_src2:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/bd3c92b0-e738-4b76-b7e5-b0990a539976?org_if_sml=13567',
+  pastedImage_alt1: 'pastedImage',
+  pastedImage_src41: '/playground_assets/pastedimage-pby9.svg',
+  pastedImage_src5:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/abb0c727-0d8c-42bd-bb5e-82972e4e7153?org_if_sml=1272',
+  pastedImage_src411: '/playground_assets/pastedimage-pby9.svg',
+  text411: 'EN',
+  pastedImage_src511:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/abb0c727-0d8c-42bd-bb5e-82972e4e7153?org_if_sml=1272',
+}
+
+Header.propTypes = {
+  pastedImage_alt4: PropTypes.string,
+  pastedImage_src51: PropTypes.string,
+  pastedImage_alt411: PropTypes.string,
+  pastedImage_alt5: PropTypes.string,
+  pastedImage_alt51: PropTypes.string,
+  text31: PropTypes.string,
+  pastedImage_src1: PropTypes.string,
+  pastedImage_alt31: PropTypes.string,
+  rootClassName: PropTypes.string,
+  pastedImage_alt41: PropTypes.string,
+  pastedImage_src4: PropTypes.string,
+  text5: PropTypes.string,
+  pastedImage_src3: PropTypes.string,
+  pastedImage_alt311: PropTypes.string,
+  text11: PropTypes.string,
+  pastedImage_alt3: PropTypes.string,
+  text: PropTypes.string,
+  text41: PropTypes.string,
+  text3: PropTypes.string,
+  pastedImage_src31: PropTypes.string,
+  pastedImage_alt2: PropTypes.string,
+  text2: PropTypes.string,
+  text4: PropTypes.string,
+  text21: PropTypes.string,
+  text1: PropTypes.string,
+  pastedImage_src311: PropTypes.string,
+  pastedImage_alt511: PropTypes.string,
+  pastedImage_src: PropTypes.string,
+  pastedImage_alt: PropTypes.string,
+  pastedImage_src2: PropTypes.string,
+  pastedImage_alt1: PropTypes.string,
+  pastedImage_src41: PropTypes.string,
+  pastedImage_src5: PropTypes.string,
+  pastedImage_src411: PropTypes.string,
+  text411: PropTypes.string,
+  pastedImage_src511: PropTypes.string,
+}
+
+export default Header
