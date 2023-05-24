e01bba5411f31fc83e0e2d9be11962af58a1d934
lydia nathania <lydia@diharjasoftware.com>
2023-05-24 14:30:59 +0700

    update image and header

--- src/about.js
+++ src/about.js
@@ -1,17 +1,85 @@
 import React from 'react'
 
-import { Helmet } from 'react-helmet'
-
+import Header from './header'
+import Footer from './footer'
 import './about.css'
+import './style.css'
 
 const About = (props) => {
   return (
     <div className="about-container">
-      <Helmet>
-        <title>about - Discrete Frivolous Bee</title>
-        <meta property="og:title" content="about - Discrete Frivolous Bee" />
-      </Helmet>
-      <div className="about-container1"></div>
+      <Header rootClassName="header-root-class-name1"></Header>
+      <div className="about-title">
+        <h1 className="about-text">About Us</h1>
+        <div className="about-container1"></div>
+      </div>
+      <div className="about-our-story">
+        <div className="about-container2">
+          <div className="about-container3">
+            <h1 className="about-text01">Our Story</h1>
+            <span className="about-text02">
+              <span>
+                With more than 200 fashion &amp; lifestyle tenants, Living World
+                delivers different shopping experience to customers by bringing
+                exciting tenants. We also have a lot of programs and events.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <br></br>
+              <span>
+                If you are interested to be partnered with us, for further
+                information, please contact us.With more than 200 fashion &amp;
+                lifestyle tenants, Living World delivers different shopping
+                experience to customers by bringing exciting tenants. We also
+                have a lot of programs and events.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <br></br>
+              <br></br>
+              <span>
+                If you are interested to be partnered with us, for further
+                information, please contact us.
+              </span>
+            </span>
+          </div>
+          <div className="about-container4">
+            <img
+              alt="pastedImage"
+              src="/playground_assets/about-our-story.png"
+              className="about-pasted-image"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="about-container5">
+        <div className="about-container6">
+          <div className="about-container7">
+            <span className="about-text10">Vision</span>
+            <span className="about-text11">
+              With more than 200 fashion &amp; lifestyle tenants, Living World
+              delivers different shopping experience to customers by bringing
+              exciting tenants.
+            </span>
+          </div>
+          <div className="about-container8">
+            <span className="about-text12">Mission</span>
+            <span className="about-text13">
+              With more than 200 fashion &amp; lifestyle tenants, Living World
+              delivers different shopping experience to customers by bringing
+              exciting tenants.
+            </span>
+          </div>
+        </div>
+      </div>
+      <Footer rootClassName="footer-root-class-name"></Footer>
     </div>
   )
 }
--- src/header.js
+++ src/header.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
 
-import "./header.css";
+import './header.css'
 
 const Header = (props) => {
   return (
@@ -25,14 +26,18 @@ const Header = (props) => {
             className="header-nav"
           >
             <div className="header-container">
-              <span className="header-text">{props.text5}</span>
-              <span className="header-text01">{props.text11}</span>
-              <span className="header-text02">{props.text21}</span>
-              <span className="header-text03">{props.text31}</span>
+              <Link to="/" className="header-navlink">
+                {props.text5}
+              </Link>
+              <Link to="/about" className="header-navlink1">
+                {props.text11}
+              </Link>
+              <span className="header-text">{props.text21}</span>
+              <span className="header-text1">{props.text31}</span>
             </div>
             <div className="header-container1">
               <div className="header-container2">
-                <span className="header-text04">{props.text41}</span>
+                <span className="header-text2">{props.text41}</span>
                 <svg viewBox="0 0 1024 1024" className="header-icon">
                   <path
                     d="M316 366l196 196 196-196 60 60-256 256-256-256z"
@@ -82,67 +87,75 @@ const Header = (props) => {
               data-role="Nav"
               className="header-nav2"
             >
-              <span className="header-text05">Home</span>
-              <span className="header-text06">About Us</span>
-              <span className="header-text07">Our Malls</span>
-              <span className="header-text08">Our Projects</span>
-              <span className="header-text09">Language</span>
-              <span className="header-text10">My Profile</span>
+              <Link to="/home" className="header-navlink2">
+                Home
+              </Link>
+              <Link to="/about" className="header-navlink3">
+                About Us
+              </Link>
+              <span className="header-text3">Our Malls</span>
+              <span className="header-text4">Our Projects</span>
+              <span className="header-text5">Language</span>
+              <span className="header-text6">My Profile</span>
             </nav>
           </div>
         </div>
       </header>
     </header>
-  );
-};
+  )
+}
 
 Header.defaultProps = {
-  pastedImage_alt4: "pastedImage",
+  pastedImage_alt4: 'pastedImage',
   pastedImage_src51:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/abb0c727-0d8c-42bd-bb5e-82972e4e7153?org_if_sml=1272",
-  pastedImage_alt411: "pastedImage",
-  pastedImage_alt5: "pastedImage",
-  pastedImage_alt51: "pastedImage",
-  text31: "Our Projects",
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/abb0c727-0d8c-42bd-bb5e-82972e4e7153?org_if_sml=1272',
+  pastedImage_alt411: 'pastedImage',
+  pastedImage_alt5: 'pastedImage',
+  pastedImage_alt51: 'pastedImage',
+  text31: 'Our Projects',
   pastedImage_src1:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/e65bd9c6-9590-4e2a-bc7c-cb03db23dc7a?org_if_sml=15154",
-  pastedImage_alt31: "pastedImage",
-  rootClassName: "",
-  pastedImage_alt41: "pastedImage",
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/e65bd9c6-9590-4e2a-bc7c-cb03db23dc7a?org_if_sml=15154',
+  pastedImage_alt31: 'pastedImage',
+  rootClassName: '',
+  pastedImage_alt41: 'pastedImage',
   pastedImage_src4:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/9b4dfff1-a88e-4bf6-a602-e7ed11769844?org_if_sml=15391",
-  text5: "Home",
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/9b4dfff1-a88e-4bf6-a602-e7ed11769844?org_if_sml=15391',
+  pastedImage_alt312: 'pastedImage',
+  text5: 'Home',
   pastedImage_src3:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/66579b52-3e75-4e05-aac9-5ea6cecee8a6?org_if_sml=12321",
-  pastedImage_alt311: "pastedImage",
-  text11: "About Us",
-  pastedImage_alt3: "pastedImage",
-  text: "Home",
-  text41: "EN",
-  text3: "Our Projects",
-  pastedImage_src31: "/playground_assets/pastedimage-nbw4.svg",
-  pastedImage_alt2: "pastedImage",
-  text2: "Our Malls",
-  text4: "EN",
-  text21: "Our Malls",
-  text1: "About Us",
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/66579b52-3e75-4e05-aac9-5ea6cecee8a6?org_if_sml=12321',
+  pastedImage_alt311: 'pastedImage',
+  text11: 'About Us',
+  pastedImage_alt3: 'pastedImage',
+  text: 'Home',
+  text41: 'EN',
+  text3: 'Our Projects',
+  pastedImage_src31: '/pastedimage-nbw4.svg',
+  pastedImage_alt2: 'pastedImage',
+  text412: 'EN',
+  text2: 'Our Malls',
+  text4: 'EN',
+  text21: 'Our Malls',
+  text1: 'About Us',
   pastedImage_src311:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/66579b52-3e75-4e05-aac9-5ea6cecee8a6?org_if_sml=12321",
-  pastedImage_alt511: "pastedImage",
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/66579b52-3e75-4e05-aac9-5ea6cecee8a6?org_if_sml=12321',
+  pastedImage_alt511: 'pastedImage',
   pastedImage_src:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/d559717d-77ef-474b-8d3d-7d0426f391e2?org_if_sml=16689",
-  pastedImage_alt: "pastedImage",
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/d559717d-77ef-474b-8d3d-7d0426f391e2?org_if_sml=16689',
+  pastedImage_alt: 'pastedImage',
   pastedImage_src2:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/bd3c92b0-e738-4b76-b7e5-b0990a539976?org_if_sml=13567",
-  pastedImage_alt1: "pastedImage",
-  pastedImage_src41: "/playground_assets/pastedimage-pby9.svg",
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/bd3c92b0-e738-4b76-b7e5-b0990a539976?org_if_sml=13567',
+  pastedImage_alt1: 'pastedImage',
+  pastedImage_src312:
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/66579b52-3e75-4e05-aac9-5ea6cecee8a6?org_if_sml=12321',
+  pastedImage_src41: '/pastedimage-pby9.svg',
   pastedImage_src5:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/abb0c727-0d8c-42bd-bb5e-82972e4e7153?org_if_sml=1272",
-  pastedImage_src411: "/playground_assets/pastedimage-pby9.svg",
-  text411: "EN",
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/abb0c727-0d8c-42bd-bb5e-82972e4e7153?org_if_sml=1272',
+  pastedImage_src411: '/pastedimage-pby9.svg',
+  text411: 'EN',
   pastedImage_src511:
-    "https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/abb0c727-0d8c-42bd-bb5e-82972e4e7153?org_if_sml=1272",
-};
+    'https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/49b33f8b-5a92-44b5-8b0f-e873f58ce5ea/abb0c727-0d8c-42bd-bb5e-82972e4e7153?org_if_sml=1272',
+}
 
 Header.propTypes = {
   pastedImage_alt4: PropTypes.string,
@@ -156,6 +169,7 @@ Header.propTypes = {
   rootClassName: PropTypes.string,
   pastedImage_alt41: PropTypes.string,
   pastedImage_src4: PropTypes.string,
+  pastedImage_alt312: PropTypes.string,
   text5: PropTypes.string,
   pastedImage_src3: PropTypes.string,
   pastedImage_alt311: PropTypes.string,
@@ -166,6 +180,7 @@ Header.propTypes = {
   text3: PropTypes.string,
   pastedImage_src31: PropTypes.string,
   pastedImage_alt2: PropTypes.string,
+  text412: PropTypes.string,
   text2: PropTypes.string,
   text4: PropTypes.string,
   text21: PropTypes.string,
@@ -176,11 +191,12 @@ Header.propTypes = {
   pastedImage_alt: PropTypes.string,
   pastedImage_src2: PropTypes.string,
   pastedImage_alt1: PropTypes.string,
+  pastedImage_src312: PropTypes.string,
   pastedImage_src41: PropTypes.string,
   pastedImage_src5: PropTypes.string,
   pastedImage_src411: PropTypes.string,
   text411: PropTypes.string,
   pastedImage_src511: PropTypes.string,
-};
+}
 
-export default Header;
+export default Header
